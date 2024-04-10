5b7f7eb38360fbdc2f0f18419d686941b655f069
psmyrdek <przemek.smyrdek@gmail.com>
2024-04-10 12:46:12 +0200

    feat: fix config for e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: process.env.E2E_BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.CI ? process.env.E2E_BASE_URL : 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
